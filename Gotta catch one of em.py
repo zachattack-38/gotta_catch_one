30afd6e61024b91eae48c30d0619a21b017c5dc8
jackiscooler <94867865+jackiscooler@users.noreply.github.com>
2021-11-22 13:47:01 -0600

    Add files via upload

---
+++ Gotta catch one of em.py
@@ -0,0 +1,11 @@
+# ----------------functions -------------------
+# rules
+# main menu
+# update_pokemon_file
+# show current pokemon
+# level up 
+# get pokemon info
+# candy # generator
+# -----------------main code -------------------
+# starting menu
+
